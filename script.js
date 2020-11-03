97e5d96be7ef17e7d672d676122908c8c51bbfbd
obaidNieroukh <obaidofj@gmail.com>
2020-11-03 22:40:20 +0200

    clear and erase buttons added relate #5

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let clr = document.getElementById("clr");
 let radioB1 = document.getElementById("radioBut1");
 let radioB2 = document.getElementById("radioBut2");
 let radioB3 = document.getElementById("radioBut3");
-let randClr = "";
+let randClr = "",
+  erase = false;
 let clrS = document.getElementById("clrSelected");
 
 function drawGrid(grdSize) {
@@ -15,17 +16,21 @@ function drawGrid(grdSize) {
     divEl.style.width = 960 / grdSize + "px";
     divEl.style.height = 960 / grdSize / 1.8 + "px";
 
-    divEl.addEventListener("mouseover", function () {
-      if (radioB1.checked) {
-        //random colors
-        divEl.style.backgroundColor =
-          "#" + Math.random().toString(16).substr(-6);
-      } else if (radioB2.checked) {
-        //random color
-        divEl.style.backgroundColor = randClr;
-      } else if (radioB3.checked) {
-        //picked color
-        divEl.style.backgroundColor = clr.value;
+    divEl.addEventListener("mouseover", function (e) {
+      if (erase == true) {
+        e.target.style.backgroundColor = "";
+      } else {
+        if (radioB1.checked) {
+          //random colors
+          divEl.style.backgroundColor =
+            "#" + Math.random().toString(16).substr(-6);
+        } else if (radioB2.checked) {
+          //random color
+          divEl.style.backgroundColor = randClr;
+        } else if (radioB3.checked) {
+          //picked color
+          divEl.style.backgroundColor = clr.value;
+        }
       }
     });
     rootDiv.appendChild(divEl);
@@ -45,8 +50,7 @@ document.getElementById("newGridBut").addEventListener("click", function () {
       ok = true;
     }
   } while (ok == false);
-
-  gridsize = valu;
+  if (valu != 0) gridsize = valu;
 
   drawGrid(gridsize);
 });
@@ -64,4 +68,14 @@ clrS.addEventListener("click", function () {
   randClr = "#" + Math.random().toString(16).substr(-6);
   clrS.style.backgroundColor = randClr;
   radioB2.checked = true;
+});
+
+document.getElementById("clearBut").addEventListener("click", function () {
+  drawGrid(gridsize);
+});
+
+let era = document.getElementById("erase");
+era.addEventListener("click", function () {
+  if (era.checked == true) erase = true;
+  else erase = false;
 });
